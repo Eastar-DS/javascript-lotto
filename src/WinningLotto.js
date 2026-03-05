bfbec279fa469a154f5f9d347dfc097dc10d5c8d
jebiyeon02 <jhje5595@naver.com>
2026-03-05 18:46:02 +0900

    feat: 보너스 번호 검증

--- src/WinningLotto.js
+++ src/WinningLotto.js
@@ -6,6 +6,15 @@ class WinningLotto {
 
   constructor(winningNumbers, bonusNumber) {
     this.#lotto = new Lotto(winningNumbers);
+    if (bonusNumber <= 0) {
+      throw new Error("[ERROR]");
+    }
+    if (bonusNumber < 1 || bonusNumber > 45) {
+      throw new Error("[ERROR]");
+    }
+    if (this.#lotto.getNumbers().includes(bonusNumber)) {
+      throw new Error("[ERROR]");
+    }
     this.#bonusNumber = bonusNumber;
     // this.#numbers = numbers.sort((a, b) => a - b);
   }
