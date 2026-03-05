ed99e2bbfc8946da1d8da44e7fdeb1abf0ef9d87
jebiyeon02 <jhje5595@naver.com>
2026-03-05 17:03:21 +0900

    feat: 구입 금액을 받아, 로또 개수를 계산

--- src/LottoGenerator.js
+++ src/LottoGenerator.js
@@ -1,6 +1,10 @@
 import Lotto from "./Lotto";
 
 const LottoGenerator = {
+  calculateBuyLottoCount(money) {
+    return money / 1_000;
+  },
+
   makeLottos(buyLottoCount) {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
     return Array.from({ length: buyLottoCount }, (v, i) => lotto);
