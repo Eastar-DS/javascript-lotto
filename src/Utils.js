7a6110dca367607d904d2ad7db606c0e42f36849
jebiyeon02 <jhje5595@naver.com>
2026-03-05 17:23:31 +0900

    feat: 범위 내의 무작위 정수를 특정 개수만큼 생성

--- src/Utils.js
+++ src/Utils.js
@@ -4,6 +4,12 @@ const Utils = {
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
   },
+
+  getRandomNumbers(min, max, count) {
+    return Array.from({ length: count }, (v) =>
+      Utils.getRandomNumber(min, max),
+    );
+  },
 };
 
 export default Utils;
