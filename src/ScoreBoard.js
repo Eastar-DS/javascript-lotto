b04649ed87f188c88a70ee4e79a466041d2a2eb2
jebiyeon02 <jhje5595@naver.com>
2026-03-05 20:47:17 +0900

    feat: 일치하는 번호 개수와 보너스 번호 포함 여부에 따라 등수를 계산

---
+++ src/ScoreBoard.js
@@ -0,0 +1,21 @@
+class ScoreBoard {
+  static getRank(matchCount, hasBonus) {
+    if (matchCount === 6) {
+      return 1;
+    }
+    if (matchCount === 5 && hasBonus) {
+      return 2;
+    }
+    if (matchCount === 5) {
+      return 3;
+    }
+    if (matchCount === 4) {
+      return 4;
+    }
+    if (matchCount === 3) {
+      return 5;
+    }
+  }
+}
+
+export default ScoreBoard;
