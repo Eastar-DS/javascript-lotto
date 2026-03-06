0921c35e3fc29b2b35d15de88be9c19b04cce02e
jebiyeon02 <jhje5595@naver.com>
2026-03-06 16:09:07 +0900

    feat: 입력중 에러 발생시 해당 지점부터 다시 입력

--- src/step1-index.js
+++ src/step1-index.js
@@ -12,7 +12,8 @@ import WinningLotto from "./WinningLotto.js";
 
 class App {
   static async run() {
-    const money = await InputView.readMoney("> 구입금액을 입력해 주세요.");
+    const money = await App.readMoneyUntilCorrect();
+
     const buyLottoCount = money / LOTTO.PRICE;
     OutputView.printBuyLottoCount(buyLottoCount);
 
@@ -21,13 +22,10 @@ class App {
     lottos.forEach((lotto) => OutputView.printLottoNumbers(lotto.getNumbers()));
 
     // 당첨 번호 입력
-    const winningNumbers =
-      await InputView.readWinningNumbers("> 당첨 번호를 입력해 주세요. ");
 
-    const bonusNumber =
-      await InputView.readBonusNumber("> 보너스 번호를 입력해 주세요.");
+    const winningNumbers = await App.readWinningNumbersUntilCorrect();
 
-    const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+    const winningLotto = await App.getWinningLotto(winningNumbers);
 
     // 당첨 여부 확인
     const allRankCount = ScoreBoard.makeAllRankCount(lottos, winningLotto);
@@ -37,5 +35,53 @@ class App {
 
     OutputView.printLottoResult(allRankCount, profitRate);
   }
+
+  static async readMoneyUntilCorrect() {
+    try {
+      const input = await InputView.readMoney("> 구입금액을 입력해 주세요.");
+
+      return input;
+    } catch (error) {
+      console.log(error.message);
+      return await App.readMoneyUntilCorrect();
+    }
+  }
+
+  static async readWinningNumbersUntilCorrect() {
+    try {
+      const winningNumbers =
+        await InputView.readWinningNumbers("> 당첨 번호를 입력해 주세요. ");
+
+      return winningNumbers;
+    } catch (error) {
+      console.log(error.message);
+      return await App.readWinningNumbersUntilCorrect();
+    }
+  }
+
+  static async readBonusNumberUntilCorrect() {
+    try {
+      const bonusNumber =
+        await InputView.readBonusNumber("> 보너스 번호를 입력해 주세요.");
+
+      return bonusNumber;
+    } catch (error) {
+      console.log(error.message);
+      return await App.readBonusNumberUntilCorrect();
+    }
+  }
+
+  static async getWinningLotto(winningNumbers) {
+    try {
+      const bonusNumber = await App.readBonusNumberUntilCorrect();
+      const winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+      return winningLotto;
+    } catch (error) {
+      console.log(error.message);
+      return await App.getWinningLotto(winningNumbers);
+    }
+  }
 }
+
 await App.run();
